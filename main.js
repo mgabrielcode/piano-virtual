3eeb846163814bbf481d2aa9c01ef1b9342b346d
Gabriel <marcosgabriel.mg318@gmail.com>
2024-03-04 17:39:55 -0300

    Adicionando comportamento de tecla pressionada

--- main.js
+++ main.js
@@ -9,19 +9,42 @@ function playNote(event){
     if(cantFoundAnyKey) {
         return;
     }
+
+    addPlayingClass(key);
+    playAudio(audioKeyCode);
+}
+
+function addPlayingClass(key){
+    key.classList.add("playing");
 }
 
 function getKeyCode(event){
     let keyCode;
 
     const isKeyboard = event.type === "keydown";
+    if(isKeyboard) {
+        keyCode = event.keyCode;
+    } else {
+        keyCode = event.target.dataset.key;
+    }
 
-    is
+    return keyCode;
+}
+
+function playAudio(audioKeyCode){
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`);
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function removePlayingclass(event){
+    event.target.classList.remove("playing");
 }
 
 
 keys.forEach(function(key){
     key.addEventListener("click", playNote);
+    key.addEventListener("transitionend", removePlayingclass)
 })
 
 window.addEventListener("keydown", playNote);
